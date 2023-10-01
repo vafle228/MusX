ec4507ffb809160464e21755341d9769816082c5
~ Vafle ~ <60180871+NeiroFucker@users.noreply.github.com>
2023-10-01 19:40:54 +0500

    Подготовка мультимодульной архитектуры
    Настроил pom файлы для мультимодульного проекта

---
+++ api/src/main/java/org/musxteam/api/Test.java
@@ -0,0 +1,7 @@
+package org.musxteam.api;
+
+public class Test {
+    public void foo() {
+        System.out.println("Hello from api");
+    }
+}
--- telegram/src/main/java/org/musxteam/telegram/Main.java
+++ telegram/src/main/java/org/musxteam/telegram/Main.java
@@ -1,7 +1,11 @@
-package org.musxteam;
+package org.musxteam.telegram;
+
+import org.musxteam.api.Test;
 
 public class Main {
     public static void main(String[] args) {
+        Test test = new Test();
+        test.foo();
         System.out.println("Hello world!");
     }
 }
